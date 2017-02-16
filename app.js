5b5dca706c583ae4ba614a9c91b9ba2a64553491
mrt-jnz <nfksgvn@gmail.com>
2017-02-16 17:12:14 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const passport = require('passport');
+const mongoose = require('mongoose');
+const users = require('./routes/users');
+const config = require('./config/database');
+
+//Connect to Database
+mongoose.connect(config.database);
+
+//On Connection
+mongoose.connection.on('connected', () => {
+    console.log('Connected to '+config.database);
+})
+
+//On Error
+mongoose.connection.on('error', (err) => {
+    console.log('Error '+err);
+})
+
+const app = express();
+
+//Port Number
+const port = 3000;
+
+//Cors Middleware
+app.use(cors());
+
+//Body Parser Middleware
+app.use(bodyParser.json());
+
+//Express Static Folder
+app.use(express.static(path.join(__dirname, '/public')));
+
+//Users Middleware
+app.use('/users', users);
+
+//Index Route
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+})
+
+//Start Server
+app.listen(port, () => {
+    console.log('server started on port 3000');
+})
